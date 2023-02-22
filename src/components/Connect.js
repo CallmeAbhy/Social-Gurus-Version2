fb81e7a1be3c282e683dfe4082d070be07b7b512
CallmeAbhy <99988396+CallmeAbhy@users.noreply.github.com>
2023-02-22 12:50:41 +0530

    Update

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,28 +1,69 @@
-import React from 'react';
-import Footer from './Footer'
 
-import NavBar from './NavBar';
+import React, { useState } from "react";
+import { io } from "socket.io-client";
+import './Speak.css'
+import Speak from "./Speak";
+import Nav_Agencies from "./Nav_Agencies";
+import NavBar2 from "./NavBar2";
+import Footer from "./Footer";
 
-const Connect = () => {
+
+function Connect() {
+  const [username, setUsername] = useState("");
+  const [room,setRoom] = useState("")
+  const [show,setShow] = useState(false)
+ 
 
 
+  const joinRoom = () => {
+    if(username !== "" && room !== ""){
+      // socket.emit("join_room", room)
+      setShow(true);
+    }
+
+  }
+  return (
+    <>
+    <Nav_Agencies />
+    <NavBar2 />
+   
+    <div className="chat-div">
+      {
+        !show ? 
+      
+      <div className="joinChatContainer">
+    <h3>Join the Chat</h3>
+    <input type="text" placeholder="Username  "
+    onChange={(event) =>
+    {
+      setUsername(event.target.value);
+    }
+    }
+    />
+    <input type="text" placeholder="Room "
+  onChange={(event) =>
+    {
+      setRoom(event.target.value);
+    }
+    }
+    />
+    <button onClick={joinRoom}>Connect</button>
+    </div>
+:
+
+
+    <Speak socket={ ""} username={username} room={room} />
+
+
+  }
 
 
     
-   
-  
-  return (
-
-
-
-
-    <div>
-      <NavBar />
-
-      
-      <Footer />
+    
     </div>
+    <Footer />
+    </>
   )
 }
 
-export default Connect
+export default Connect;
